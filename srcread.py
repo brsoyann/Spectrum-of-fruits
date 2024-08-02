00a83307f88b5aa903e907dba6a7f2a3bfc21c03
dzed05 <karapetianeduard05@gmail.com>
2024-08-02 13:04:09 +0400

    [Feat] Removed findparameterByType() method from srcread.py and implemented the same functionality using classes.

--- srcread.py
+++ srcread.py
@@ -1,4 +1,14 @@
-def findparameterByType(linesList, parameterType):
-    for line in linesList:
-        if line.startswith(parameterType + ': '):
-            return line[line.find(' ')+1:]
+class SRCRead:
+
+    def __init__(self, file):
+
+        self.file = file
+
+    def findParameterByPath(self, parameterType):
+
+        with open(self.file, 'r') as src:
+            linesList = src.readlines()
+        
+        for line in linesList:
+            if line.startswith(parameterType + ': '):
+                return line[line.find(' ') + 1:]
